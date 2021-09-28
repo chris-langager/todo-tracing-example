ac0dbfe50a3dabddf0d0e51b98a5ae85e334e5e1
Chris Langager <chris.langager@shopify.com>
2021-09-28 15:00:42 -0600

    board filtering

--- simple-app/src/api/todos/listTodos.ts
+++ simple-app/src/api/todos/listTodos.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from 'express';
 import * as store from '../../store';
 
 export const listTodos: RequestHandler = async (req, res) => {
-  const { todos } = await store.listTodos();
+  const boardId = req.query.boardId as string;
+  const { todos } = await store.listTodos({ boardIds: boardId && [boardId] });
   res.json({ data: todos });
 };
